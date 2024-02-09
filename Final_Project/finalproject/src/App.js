14b688f3f6d3c1b46badb57ab9911cf3d07d1843
Atharv Patil <atharvpatil0@gmail.com>
2024-02-09 12:19:12 +0530

    Login

--- Final_Project/finalproject/src/App.js
+++ Final_Project/finalproject/src/App.js
@@ -3,11 +3,13 @@ import Registration from './components/Registration';
 import { Link, Routes, Route } from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
+import { Login } from './components/Login';
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/register_page" element={<Registration />} />
+        <Route path="/login_page" element={<Login />} />
       </Routes>
     </div>
   );
